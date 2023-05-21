d7e80158c0d7a717f7a13d3d694cc8e0d6f66d66
AlexeiRomanchenko <119013069+AlexeiRomanchenko@users.noreply.github.com>
2023-05-22 01:49:08 +0300

    fix: Добавить тесты для Update и Get

--- src/test/java/ru/yandex/practicum/filmorate/FilmsTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmsTest.java
@@ -101,4 +101,48 @@ public class FilmsTest {
         Throwable exception = assertThrows(ValidationException.class, () -> filmController.create(film));
         assertEquals(LogMessagesUsers.VALIDATION_FAILED.getMessage(), exception.getMessage());
     }
+
+    @Test
+    void shouldUpdateFilm() {
+        Film film = Film.builder()
+                .id(1)
+                .name("Агент007")
+                .description("Джеймс Бонд")
+                .duration(9879)
+                .releaseDate(LocalDate.of(1895, 12, 29))
+                .build();
+        filmController.create(film);
+
+        film.setName("Агент009");
+        film.setDescription("Бонд");
+        film.setDuration(777);
+        filmController.update(film);
+        assertEquals("[Film(id=1, name=Агент009, description=Бонд, releaseDate=1895-12-29, duration=777)]",
+                String.valueOf(filmController.getFilms()));
+    }
+
+    @Test
+    void shouldGetFilms() {
+        Film film = Film.builder()
+                .id(1)
+                .name("Агент007")
+                .description("Джеймс Бонд")
+                .duration(9879)
+                .releaseDate(LocalDate.of(1895, 12, 29))
+                .build();
+        filmController.create(film);
+
+        Film film1 = Film.builder()
+                .id(2)
+                .name("Аватар")
+                .description("Фантастика")
+                .duration(7559)
+                .releaseDate(LocalDate.of(2001, 1, 2))
+                .build();
+        filmController.create(film1);
+
+        assertEquals("[Film(id=1, name=Агент007, description=Джеймс Бонд, releaseDate=1895-12-29, duration=9879)," +
+                        " Film(id=2, name=Аватар, description=Фантастика, releaseDate=2001-01-02, duration=7559)]",
+                String.valueOf(filmController.getFilms()));
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -81,4 +81,52 @@ public class UserTest {
         assertEquals("[User(id=1, email=privet@ya.ru, login=Andry, name=Andry, birthday=1958-12-30)]",
                 String.valueOf(userController.getUsers()));
     }
+    @Test
+    void shouldUpdateUser() {
+        User user = User.builder()
+                .id(1)
+                .name("Иван")
+                .email("ivan@rjd.ru")
+                .login("Vanya")
+                .birthday(LocalDate.of(1955, 6, 19))
+                .build();
+        userController.create(user);
+
+        assertEquals("[User(id=1, email=ivan@rjd.ru, login=Vanya, name=Иван, birthday=1955-06-19)]",
+                String.valueOf(userController.getUsers()));
+
+        user.setName("Николай");
+        user.setLogin("Kolya");
+        user.setEmail("kolya@rjd.ru");
+
+        userController.update(user);
+        assertEquals("[User(id=1, email=kolya@rjd.ru, login=Kolya, name=Николай, birthday=1955-06-19)]",
+                String.valueOf(userController.getUsers()));
+    }
+
+    @Test
+    void shouldGetUsers() {
+        User user = User.builder()
+                .id(1)
+                .name("Иван")
+                .email("ivan@rjd.ru")
+                .login("Vanya")
+                .birthday(LocalDate.of(1955, 6, 19))
+                .build();
+        userController.create(user);
+
+        User user1 = User.builder()
+                .id(2)
+                .name("Саша")
+                .email("sasha@rjd.ru")
+                .login("Sanya")
+                .birthday(LocalDate.of(1965, 6, 21))
+                .build();
+        userController.create(user1);
+
+        assertEquals("[User(id=1, email=ivan@rjd.ru, login=Vanya, name=Иван, birthday=1955-06-19), " +
+                        "User(id=2, email=sasha@rjd.ru, login=Sanya, name=Саша, birthday=1965-06-21)]",
+                String.valueOf(userController.getUsers()));
+
+    }
 }
