5df41a78b23054a134ebe54cbb5b5ccc76f834cf
AlexeiRomanchenko <119013069+AlexeiRomanchenko@users.noreply.github.com>
2023-05-22 02:02:50 +0300

    fix: отправка на ревью

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmAlreadyExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmAlreadyExistException.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 public class FilmAlreadyExistException extends RuntimeException {
     public FilmAlreadyExistException() {
     }
+
     public FilmAlreadyExistException(String message) {
         super(message);
     }
