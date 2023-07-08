a759e01b762934757a94d7950ef040a8146cdfb9
ARGalimov <ARGalimov87@yandex.ru>
2023-07-08 13:47:55 +0500

    Разработать функциональность "Лента событий" V1

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.description.LogMessagesUsers;
-import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.model.Event;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.interfaces.EventStorage;
 import ru.yandex.practicum.filmorate.description.EventType;
 import ru.yandex.practicum.filmorate.description.Operation;
