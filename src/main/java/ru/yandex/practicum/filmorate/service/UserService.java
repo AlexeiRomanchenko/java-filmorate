acfa88913a3b471d4e42cc8e13389a64afabe150
anton-tt <carduelis@yandex.ru>
2023-07-13 14:46:30 +0700

    Тест-2

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -42,6 +42,9 @@ public class UserService {
     }
 
     public void deleteUser(int id) {
+        if (id < 0) {
+            throw new ObjectNotFoundException(LogMessagesUsers.ID_NOT_POSITIVE.getMessage());
+        }
         userStorage.delete(id);
     }
 
