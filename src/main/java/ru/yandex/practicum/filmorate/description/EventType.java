0d2a60d5c2eadf1696e7a942fa776a1be9404c66
ARGalimov <ARGalimov87@yandex.ru>
2023-07-17 11:05:17 +0500

    Исправление замечаний ревьювера в функциональностях "Отзывы" и "Лента событий"

--- src/main/java/ru/yandex/practicum/filmorate/description/EventType.java
+++ src/main/java/ru/yandex/practicum/filmorate/description/EventType.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.filmorate.description;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum EventType {
     LIKE("LIKE"),
     REVIEW("REVIEW"),
     FRIEND("FRIEND");
     private final String event;
-
-    EventType(String event) {
-        this.event = event;
-    }
 }
