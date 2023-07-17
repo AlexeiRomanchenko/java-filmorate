9e0a7addd365bc781dedb63a8b9f42d4cf1e9ea8
AlexeiRomanchenko <119013069+AlexeiRomanchenko@users.noreply.github.com>
2023-07-17 15:24:39 +0300

    fix: Добавлена анатация @Positive. Удалены лишние поля. Исправлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.description.LogMessagesUsers;
 import ru.yandex.practicum.filmorate.model.Event;
